3a68452d604da18b2df9854cef5f84a30ece3a04
Jansen Gabrillo <Gabrillojansen@gmail.com>
2024-05-24 22:53:47 +0800

    Added navigation bar active state

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,26 +1,19 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}">
-                <i class="bi bi-grid"></i>
-                <span>{{ __('Dashboard') }}</span>
+            <a class="nav-link" href="{{ route('dashboard') }}" style="{{ request()->routeIs('dashboard') ? '' : 'background-color: #fff; color: #012970;' }}">
+                <i class="bi bi-grid" style="{{ request()->routeIs('dashboard') ? '' : 'color: #7B7D7D;' }}"></i><span>{{ __('Dashboard') }}</span>
             </a>
         </li>
 
         <li class="nav-item">
-            <a
-                class="nav-link collapsed"
-                data-bs-target="#resources-nav"
-                data-bs-toggle="collapse"
-                href="#"
-            >
-                <i class="bi bi-menu-button-wide"></i><span>Resources</span
-                ><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#resources-nav" data-bs-toggle="collapse" href="#" style="{{ request()->routeIs('post.index') ? 'background-color: #f6f9ff; color: #4154f1;' : '' }}">
+                <i class="bi bi-menu-button-wide" style="{{ request()->routeIs('post.index') ? 'color: #4154f1;' : '' }}"></i><span>{{ __('Resources') }}</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
             <ul id="resources-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="{{ route('post.index') }}">
-                        <i class="bi bi-circle"></i><span>Posts</span>
+                    <a href="{{ route('post.index') }}" style="{{ request()->routeIs('post.index') ? 'color: #4154f1;' : '' }}">
+                        <i class="bi bi-circle" ></i><span>{{ __('Posts') }}</span>
                     </a>
                 </li>
             </ul>
